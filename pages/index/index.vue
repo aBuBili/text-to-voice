be135d2ea2b7df88ccaa958635669b477ba793df
aBuBili <70617237+aBuBili@users.noreply.github.com>
2022-07-29 22:31:12 +0800

    feat: 新UI

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,12 +1,37 @@
 <template>
 	<view class="content">
-		<p class="title"> (๑•̀ㅂ•́)و✧ </p>
-		<textarea v-model="content" class="areaBox" @blur="onEditArea" placeholder="请输入要读的文字" />
 
-		<view class="submit" @click="submit">
-			<image src="../../static/pause.png" mode="" class="laba" v-if="isPlay"></image>
-			<image src="../../static/play.png" mode="" class="laba" v-else></image>
+		<view class="profileList">
+			<view class="profile" :class="{'activePro pro0':per == 0}" @click="onChangeSpeak(0)">
+				<image src="../../static/profile-2.png">
+				</image>
+				<view>小美</view>
+			</view>
+			<view class="profile" :class="{'activePro pro1':per == 1}" @click="onChangeSpeak(1)">
+				<image src="../../static/profile-4.png">
+				</image>
+				<view>小宇</view>
+			</view>
+			<view class="profile" :class="{'activePro pro3':per == 3}" @click="onChangeSpeak(3)">
+				<image src="../../static/profile-3.png">
+				</image>
+				<view>逍遥</view>
+			</view>
+			<view class="profile" :class="{'activePro pro4':per == 4}" @click="onChangeSpeak(4)">
+				<image src="../../static/profile-1.png">
+				</image>
+				<view>丫丫</view>
+			</view>
 		</view>
+
+		<view class="whiteBox">
+			<textarea v-model="content" class="inputArea" @blur="onEditArea()" placeholder="请输入要读的文字" />
+			<view class="playBtn" @click="submit()">
+				<image src="../../static/pauseVoice.png" class="playIcon" v-if="status == 2"></image>
+				<image src="../../static/playVoice.png" class="playIcon" v-else></image>
+			</view>
+		</view>
+
 	</view>
 </template>
 
@@ -20,10 +45,14 @@
 				// token 有效期30天 
 				access_token: "",
 				isPlay: false, //当前音频是否正在播放 play|pause
-				isEnd: true //当前音频播放完毕
+				isEnd: true, //当前音频播放完毕
+				per: 0, //朗读者 默认是小美
+
+				status: 4, //2=播放中3=暂停4=播放完毕
 			}
 		},
 		onLoad() {
+			// 语音合成项目的key和密钥
 			const appKey = '7Bm6Lj5hgVNdC3UTRG2jBmCM'
 			const appSecret = 'gxGGih19NRI8OAxiemPxdhQ1v9onDYxZ'
 			// 获取 token
@@ -35,6 +64,7 @@
 			})
 		},
 		onShow() {
+			// 获取剪切板的文字
 			uni.getClipboardData({
 				success: (res) => {
 					this.content = res.data || '您还没有输入文字'
@@ -43,16 +73,19 @@
 			});
 		},
 		methods: {
+			// 点击播放按钮
 			async submit() {
-				if (this.isEnd) {
-					this.isEnd = false
-					this.isPlay = true
-					this.audio = uni.createInnerAudioContext();
-					this.audio.loop = false
+				if (this.status == 4) {
+					console.log('结束或有更改 重新请求')
+					this.status = 2
+					this.audio = uni.createInnerAudioContext(); // 音频对象
+					this.audio.loop = false //播放结束不循环播放 未生效
 
+					// 生成 获取音频的url和参数
 					const url = ''
-					const params = [`tok=${this.access_token}`, `tex=${encodeURIComponent(this.content || '您还没有输入文字')}`, 'spd=5',
-						'put=5', 'vol=5', 'pre=1', 'ctp=1', 'lan=zh', 'aue=3', 'cuid=abuuio'
+					const params = [`tok=${this.access_token}`,
+						`tex=${encodeURIComponent(this.content || '您还没有输入文字')}`, 'spd=5',
+						'put=5', 'vol=5', 'per=' + this.per, 'ctp=1', 'lan=zh', 'aue=3', 'cuid=abuuio'
 					]
 
 					this.audio.src = `https://tsn.baidu.com/text2audio?${params.join('&')}`
@@ -66,29 +99,29 @@
 					// 自然播放结束
 					this.audio.onEnded(() => {
 						console.log('播放结束')
-						this.isEnd = true
-						this.isPlay = false
-						this.audio.destroy()
+						this.status = 4
+						this.audio.destroy() //销毁 因为有继续自动播放的bug
 					})
 
-				} else {
-					// 暂停就播放 播放就暂停
-					if (this.audio.paused) {
-						console.log('播放')
-						this.audio.play()
-						this.isPlay = true
-					} else {
-						console.log('暂停')
-						this.audio.pause()
-						this.isPlay = false
-					}
+				} else if (this.status == 2) {
+					console.log('播放状态 去暂停')
+					this.audio.pause()
+					this.status = 3
+				} else if (this.status == 3) {
+					console.log('暂停状态 去播放')
+					this.audio.play()
+					this.status = 2
 				}
 			},
 			// 更改输入框的内容 重新获取音频
 			onEditArea() {
-				this.isEnd = true
-				this.isPlay = false
 				this.audio.destroy()
+				this.status = 4
+			},
+			// 更改朗读的人
+			onChangeSpeak(val) {
+				this.per = val
+				this.status = 4
 			}
 		}
 	}
@@ -96,38 +129,93 @@
 
 <style>
 	.content {
-		padding: 16px;
+		background-color: #FAFAFA;
+		height: 100vh;
 	}
 
-	.title {
-		color: #333;
-		font-size: 14px;
-		margin: 64px 0 16px 0;
+	.profileList {
+		padding-left: 30upx;
+		padding-top: 240upx;
+		display: flex;
+		align-items: center;
+		transition: all .2s;
 	}
 
-	.areaBox {
-		height: 320px;
-		background-color: rgba(0, 0, 0, 0.04);
-		border-radius: 32px;
-		padding: 16px;
-		font-size: 14px;
-		color: #333;
+	.profile {
+		border: 2px solid #FAFAFA;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		opacity: 0.4;
+		padding: 10upx 30upx;
 	}
 
-	.submit {
-		width: 120px;
-		height: 80px;
-		background-color: #2C6CCB;
-		border-radius: 50px;
-		box-shadow: 12px 12px 24px #2C6CCB88;
+	.activePro {
+		border-radius: 16upx;
+		transition: all .2s;
+		opacity: 1;
+		padding: 10upx 30upx;
+	}
+
+	.pro0 {
+		border: 2px solid #eab6b4;
+	}
+
+	.pro1 {
+		border: 2px solid #acd7f3;
+	}
+
+	.pro3 {
+		border: 2px solid #afd3b1;
+	}
+
+	.pro4 {
+		border: 2px solid #f5d284;
+	}
+
+	.profile image {
+		width: 100upx;
+		height: 100upx;
+	}
+
+	.profile view {
+		font-size: 20upx;
+		color: #707070;
+		margin-top: 10upx;
+	}
+
+	.whiteBox {
+		margin-top: 50upx;
+		background-color: #fff;
+		border-radius: 60upx;
+		padding: 40upx;
+	}
+
+
+	.inputArea {
+		background-color: #F4F4F4;
+		color: #A6A6A6;
+		font-size: 32upx;
+		border-radius: 48upx;
+		width: calc(100% - 64upx);
+		height: 418upx;
+		padding: 32upx;
+
+		font-size: 28upx;
+	}
+
+	.playBtn {
+		width: 320upx;
+		height: 160upx;
+		background-color: #383838;
+		border-radius: 240upx;
 		text-align: center;
-		line-height: 110px;
-		margin-left: calc(50% - 60px);
-		margin-top: 40px;
+		line-height: 200upx;
+		margin: 120upx auto;
 	}
 
-	.laba {
-		width: 41px;
-		height: 40px;
+	.playIcon {
+		width: 64upx;
+		height: 64upx;
 	}
 </style>
